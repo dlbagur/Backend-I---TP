74d591b39dff26ff38d790a45cce17ee3210f4b6
dlbagur <dlbagur@hotmail.com>
2024-08-14 20:01:02 -0300

    Backend I Trabajo Practico - V2.00

--- src/dao/VinosManager.js
+++ src/dao/VinosManager.js
@@ -1,13 +1,60 @@
-import { existsSync, promises } from "fs"
+import fs from "fs"
 class VinosManager{
     static path
 
     static async getVinos(){
-        if(existsSync(this.path)){
-            return JSON.parse(await promises.readFile(this.path, {encoding:"utf-8"}))
+        if(fs.existsSync(this.path)){
+            let vinos=JSON.parse(await fs.promises.readFile(this.path, {encoding:"utf-8"}))
+            return vinos
         }else{
             return []
         }
+    }
+
+    static async addVino(vino={}){
+        let vinos=await this.getVinos()
+        let id=1
+        if(vinos.length>0){
+            id=Math.max(...vinos.map(v=>v.id))+1
+        }
+        let nuevoVino={
+            id,
+            ...vino
+        }
+        vinos.push(nuevoVino)
+        await fs.promises.writeFile(this.path, JSON.stringify(vinos, null, 5))
+
+        return nuevoVino
+    }
+
+    static async updateVino(id, aModificar={}){
+        let vinos=await this.getVinos()
+        let indiceVino=vinos.findIndex(h=>h.id===id)
+        if(indiceVino===-1){
+            throw new Error(`Error: no existe el id: ${id}`)
+        }
+        vinos[indiceVino]={
+            ...vinos[indiceVino],
+            ...aModificar,
+            id
+        }
+        await fs.promises.writeFile(this.path, JSON.stringify(vinos, null, 5))
+        return vinos[indiceVino]
+    }
+
+    static async deleteVino(id){
+        let vinos=await this.getVinos()
+        let indiceVino=vinos.findIndex(h=>h.id===id)
+        if(indiceVino===-1){
+            throw new Error(`Error: no existe id ${id}`)
+        }
+        let cantidad0=vinos.length
+        vinos=vinos.filter(h=>h.id!==id)   
+        let cantidad1=vinos.length
+       
+        await fs.promises.writeFile(this.path, JSON.stringify(vinos, null, 5))
+
+        return cantidad0-cantidad1
     }
 }
 
---
+++ src/routes/cart.js
@@ -0,0 +1,6 @@
+import express from 'express';
+import CartManager from '../dao/CartManager.js';
+
+const router = express.Router();
+
+// Crear un nuevo carrito de compras
